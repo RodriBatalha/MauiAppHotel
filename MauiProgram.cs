308860425fc7e04eb72e58702df4858775c0d0d6
RodriBatalha <Rodrigobatalha001@gmail.com>
2024-11-10 23:39:06 -0300

    Add files via upload

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,13 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("RobotoSlab-Black.ttf", "RobotoSlabBlack");
+                    fonts.AddFont("RobotoSlab-Bold.ttf", "RobotoSlabBold");
+                    fonts.AddFont("RobotoSlab-Medium.ttf", "RobotoSlabMedium");
+                    fonts.AddFont("RobotoSlab-Regular.ttf", "RobotoSlabRegular");
+                    fonts.AddFont("RobotoSlab-Semibold.ttf", "RobotoSlabSemibold");
+
+
                 });
 
 #if DEBUG
